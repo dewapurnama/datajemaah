0f739044d2b297683e35b385915bfa73c9870a98
dewapurnama <75442383+dewapurnama@users.noreply.github.com>
2025-07-07 14:08:47 +0700

    Update jemaah.py

--- jemaah.py
+++ jemaah.py
@@ -3,43 +3,14 @@ import psycopg2
 from psycopg2 import OperationalError
 import pandas as pd
 
-# --- Function to connect and fetch data ---
-@st.cache_data(show_spinner=True)  # cache the result for faster reload
-def fetch_data():
-    try:
-        connection = psycopg2.connect(
-          host=st.secrets["db_host"],
-          database=st.secrets["db_name"],
-          user=st.secrets["db_user"],
-          password=st.secrets["db_password"],
-          port=st.secrets["db_port"]
-        )
-        cursor = connection.cursor()
-        
-        query = """
-        select * from ibbank.bankreportjournal where tx_code = 'HJOA' and kode_valuta = 'USD' and tx_date >= date '2025-06-01' and tx_date < '2025-06-30' 
-        """
-        
-        cursor.execute(query)
-        result = cursor.fetchall()
-        column_names = [col[0] for col in cursor.description]
-        df = pd.DataFrame(result, columns=column_names)
-        
-        cursor.close()
-        connection.close()
-        return df
-    
-    except OperationalError as e:
-        st.error(f"Database connection failed: {e}")
-        return pd.DataFrame()  # return empty DataFrame on error
+# streamlit_app.py
 
-# --- Streamlit App Layout ---
-st.title("📊 Jumlah Pendaftaran Jemaah")
+import streamlit as st
 
-df = fetch_data()
+# Initialize connection.
+conn = st.connection("select * from ibbank.bankreportjournal where tx_code = 'HJOA' and kode_valuta = 'USD' and tx_date >= date '2025-06-01' and tx_date < '2025-06-30' ", type="sql")
 
-if not df.empty:
-    st.success("Data fetched successfully!")
-    st.dataframe(df)
-else:
-    st.warning("No data to display.")
+# Perform query.
+df = conn.query('SELECT * FROM mytable;', ttl="10m")
+
+st.write(df)
